6c662bf5a47deb1c6b4f2f6c8e1c4e92dbf65d1b
yusuf2091 <yusufabubakar2091@gmail.com>
2024-12-24 12:17:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey="6e4ebf43e689c34268618a096ae7fa9f";
+
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response=await fetch(apiUrl+city+`&appid=${apiKey}`);
+    var data=await response.json();
+    console.log(data);
+
+    document.querySelector(".temp").innerHTML=data.main.temp+"°c";
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".humidity").innerHTML=data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed +"km/h";
+    
+
+    if(data.weather[0].main=="Clear"){
+        weatherIcon.src="./images/clear.png";
+    }
+    else if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="./images/clouds.png";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src="./images/rain.png";
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src="./images/drizzle.png";
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src="./images/mist.png";
+    }
+    
+}
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+});
